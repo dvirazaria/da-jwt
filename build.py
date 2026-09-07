ea2f8517ec6e10f4429ea586f0a08f2853f0f244
Dvir Azaria <dvirazaria1@gmail.com>
2026-09-07 17:06:28 +0300

    fix: make long press fill horizontal

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ Usage: python3 build.py
 """
 import re
 
-VERSION = 26
+VERSION = 27
 
 HEAD = """<!doctype html>
 <html lang="he" dir="rtl">
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kupa-v26";
+const CACHE = "kupa-v27";
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./"])));
