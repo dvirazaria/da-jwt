1ae2d417d93ccfca667b1d865c48233328c91ab5
Dvir Azaria <dvirazaria1@gmail.com>
2026-09-09 18:35:00 +0300

    chore: build version 58

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ Usage: python3 build.py
 """
 import re
 
-VERSION = 57
+VERSION = 58
 
 HEAD = """<!doctype html>
 <html lang="he" dir="rtl">
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kupa-v57";
+const CACHE = "kupa-v58";
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./"])));
